4520de56872fe73e63e87cca4fbd642e14396b01
Yukinari Takahashi <s21c1073ib@s.chibakoudai.jp>
2025-01-09 05:29:24 +0900

    Improve the test

--- test.bash
+++ test.bash
@@ -1,5 +1,17 @@
-#!/bin/bash
+#!/bin/bash 
+# SPDX-FileCopyrightText: 2024 Yukinari Takahashi
+# SPDX-License-Identifier: BSD-3-Clause
 
+ng (){
+	echo NG at Line $1
+	res=1
+}
+
+res=0
+
+### I/0 TEST ###
 out=$(seq 5 | ./plus)
-
-[ "${out}" = 15 ]
+[ "${out}" = 15 ] || ng "$LINENO"
+    
+[ "${res}" = 0 ] && echo OK
+exit $res
